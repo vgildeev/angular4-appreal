01e12938d8ea2a04e2202ae023aa5d152bccb35e
Slavik Gildeev <someheal@gmail.com>
2017-08-28 03:54:10 +0300

    Fixed favorite-users modal spec

--- src/app/favorite-users-modal/favorite-users-modal.component.spec.ts
+++ src/app/favorite-users-modal/favorite-users-modal.component.spec.ts
@@ -1,22 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MdDialog, MdDialogModule } from '@angular/material';
+
 import { FavoriteUsersModalComponent } from './favorite-users-modal.component';
+
+@NgModule({
+  declarations: [FavoriteUsersModalComponent],
+  imports: [
+    MdDialogModule,
+    BrowserModule,
+    NoopAnimationsModule,
+  ],
+  entryComponents: [FavoriteUsersModalComponent],
+  exports: [FavoriteUsersModalComponent],
+})
+class TestModule {}
 
 describe('FavoriteUsersModalComponent', () => {
   let component: FavoriteUsersModalComponent;
   let fixture: ComponentFixture<FavoriteUsersModalComponent>;
+  let dialog: MdDialog;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FavoriteUsersModalComponent ]
+      imports: [
+        TestModule,
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FavoriteUsersModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    dialog = TestBed.get(MdDialog);
+    let dialogRef = dialog.open(FavoriteUsersModalComponent);
+
+    component = dialogRef.componentInstance;
   });
 
   it('should be created', () => {
