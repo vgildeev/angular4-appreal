a97d4b543554fce5ded14447f25db379c6cf9c1f
Slavik Gildeev <someheal@gmail.com>
2017-08-28 03:08:18 +0300

    Fixed search-users service spec

--- src/shared/services/search-users.service.spec.ts
+++ src/shared/services/search-users.service.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { SearchUsersService } from './search-users.service';
 
 describe('SearchUsersService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+      ],
       providers: [SearchUsersService]
     });
   });
 
-  it('should ...', inject([SearchUsersService], (service: SearchUsersService) => {
+  it('should be created', inject([SearchUsersService], (service: SearchUsersService) => {
     expect(service).toBeTruthy();
   }));
 });
