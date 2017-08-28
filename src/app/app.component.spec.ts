1a80e8a096c0cbd0f92cbec3888ed6d3324ab4cd
Slavik Gildeev <someheal@gmail.com>
2017-08-28 03:59:43 +0300

    Hotfix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,7 +34,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app works!');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h2 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
